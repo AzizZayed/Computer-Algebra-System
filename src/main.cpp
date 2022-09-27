d77c25485a0bf83e2b4f6c98650d1b8283d20082
Abd-El-Aziz Zayed <azayed01@gmail.com>
2022-09-27 03:44:46 -0400

    Change cmake version

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,8 @@
 #include <io.h>
 #endif
 
-#include "cas/node/Ceil.h"
-#include "cas/node/Const.h"
-#include "cas/node/Floor.h"
-#include "cas/node/Round.h"
-#include "cas/node/Sum.h"
-#include "cas/node/Var.h"
-#include "cas/node/trig/Cos.h"
-#include "cas/node/trig/Sin.h"
+#include "cas/plot/Function.h"
 #include "cas/util/StringUtils.h"
-#include "cas/latex/LatexRenderer.h"
-#include "cas/data/ExpressionParser.h"
 
 int main() {
 
@@ -26,24 +17,8 @@ int main() {
     }
 #endif
 
-    auto* expression1 = new cas::Floor(cas::Const::PI());
-    auto* expression2 = new cas::Ceil(cas::Const::PI());
-    auto* expression3 = new cas::Round(cas::Const::PI());
-    auto* expression7 = cas::Const::PI()->cos()->subtract(cas::Const::PI()->sin());
-
-    auto* expression4 = new cas::Floor(cas::Const::PHI());
-    auto* expression5 = new cas::Ceil(cas::Const::PHI());
-    auto* expression6 = new cas::Round(cas::Const::PHI());
-    auto* expression8 = cas::Const::PHI()->sin();
-
-    wPrint(expression1->stringify());
-    wPrint(expression2->stringify());
-    wPrint(expression3->stringify());
-    wPrint(expression4->stringify());
-    wPrint(expression5->stringify());
-    wPrint(expression6->stringify());
-    wPrint(expression7->stringify());
-    wPrint(expression8->stringify());
+    cas::Function function = cas::Function::parse("cosx*sinx/tanx");
+    wprintf(L"f(x) = %ls", function.stringify().c_str());
 
     return 0;
 }
